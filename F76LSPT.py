dab7fd768977d672308e461463d4d9f671493114
texnevada <15308761+texnevada@users.noreply.github.com>
2019-09-11 18:17:59 +0200

    Added in resizing

--- F76LSPT.py
+++ F76LSPT.py
@@ -7,9 +7,10 @@ Import list
 
 from distutils.dir_util import copy_tree
 from colorama import init, Fore, Style
-import os
+import os, sys
 import glob
-import msvcrt as m
+import shutil
+from PIL import Image
 
 """
 ================
@@ -17,7 +18,7 @@ Prep variables
 ================
 """
 init()
-version = "0.1"
+version = "1.0"
 
 red = Fore.RED
 yellow = Fore.YELLOW
@@ -30,58 +31,71 @@ Code begins
 =============
 """
 
+
 def menu():
     print(yellow)
-    print("========================")
-    print("F76LM - Fo76 Loading Screen Preparing Tool ")
-    print("Version {}".format(version))
-    print("========================")
+    print("===================================")
+    print(f"F76LSPT - Version: {version}")
+    print("Fo76 Loading Screen Preparing Tool ")
+    print("===================================")
     print(reset)
+
+    if not os.path.exists("Prep"):
+        print("-Creating prep folder")
+        os.makedirs("Prep")
+    print("Please put the images you want as a loading screen for 76 in the \"Prep\" folder before you continue.")
+    input("Press ENTER to continue\n")
+    file_conversion()
+
 
 def file_conversion():
     FromDirectory = "Prep"
     ToDirectory = "Temp"
+    if not os.path.exists("temp"):
+        print(f"{yellow}-Creating temp folder{reset}")
 
-    print(yellow)
-    print(f"Copying files.\n{reset}{red}This can be fast or slow depending on the amount of files you have loaded in.{reset}")
+    print(f"Copying files.\n{red}This can be slow depending on the amount of files you have loaded in.{reset}")
 
     copy_tree(FromDirectory, ToDirectory)
 
-    print(yellow)
     print("Renaming files.")
-    print(reset, cyan)
-
-    files = glob.glob('temp\*.*')
+    files = glob.glob('temp\\*.*')
     for file in files:
-        print(f"Renaming {file}")
+        print(f"{cyan}Renaming {file}{reset}")
         parts = file.split(".png" or ".jpg")
         new_name = "{}-thumbnail.png".format(parts[0])
         os.rename(file, new_name)
 
-    print(reset, yellow)
+    print("Resizing images")
+    path = "Temp\\"
+    dirs = os.listdir("Temp")
+    for item in dirs:
+        if os.path.isfile(path + item):
+            print(f"Resizing {item}")
+            im = Image.open(path + item)
+            imResize = im.resize((480, 270), Image.ANTIALIAS)
+            imResize.save(path+item, "PNG")
 
-    TempDirectory = "Temp"
-    FinishedDirectory = "Finished"
+    if not os.path.exists("Finished"):
+        print(f"{yellow}-Creating Finished folder{reset}")
 
-    print("Copying original files")
-    copy_tree(FromDirectory, FinishedDirectory)
+    Move1 = glob.glob("Prep\\*.*")
+    Move2 = glob.glob("Temp\\*.*")
 
-    print("Copying renamed files")
-    copy_tree(TempDirectory, FinishedDirectory)
+    print("Moving files from Prep to Finished folder"+cyan)
+    for file in Move1:
+        print(f"{cyan}Moving {file} to Finished folder. {reset}")
+        shutil.move(file, "Finished")
+    print(reset+"Moved files from Prep to Finished")
 
-    cleanup1 = glob.glob("Prep\\*.*")
-    cleanup2 = glob.glob("Temp\\*.*")
+    print("Moving files from Temp to Finished folder"+cyan)
+    for file in Move2:
+        print(f"{cyan}Moving {file} to Finished folder. {reset}")
+        shutil.move(file, "Finished")
+    print(reset + "Moved files from Temp to Finished")
 
-    print("Cleaning up prep folder")
-    for file in cleanup1:
-        os.remove(file)
 
-    print("Cleaning up temp folder")
-    for file in cleanup2:
-        os.remove(file)
-
-    print(reset, green)
+    print(green)
     input("Done!\nYou can now close the program")
 
-
-file_conversion()
+menu()
