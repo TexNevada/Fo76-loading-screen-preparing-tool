153f1faf87bf599ef948d692e04e699f55571ef0
texnevada <15308761+texnevada@users.noreply.github.com>
2019-09-11 16:11:47 +0200

    Forgot to add main file. Derp

---
+++ F76LSPT.py
@@ -0,0 +1,87 @@
+
+"""
+==============
+Import list
+==============
+"""
+
+from distutils.dir_util import copy_tree
+from colorama import init, Fore, Style
+import os
+import glob
+import msvcrt as m
+
+"""
+================
+Prep variables
+================
+"""
+init()
+version = "0.1"
+
+red = Fore.RED
+yellow = Fore.YELLOW
+cyan = Fore.CYAN
+green = Fore.GREEN
+reset = Style.RESET_ALL
+"""
+=============
+Code begins
+=============
+"""
+
+def menu():
+    print(yellow)
+    print("========================")
+    print("F76LM - Fo76 Loading Screen Preparing Tool ")
+    print("Version {}".format(version))
+    print("========================")
+    print(reset)
+
+def file_conversion():
+    FromDirectory = "Prep"
+    ToDirectory = "Temp"
+
+    print(yellow)
+    print(f"Copying files.\n{reset}{red}This can be fast or slow depending on the amount of files you have loaded in.{reset}")
+
+    copy_tree(FromDirectory, ToDirectory)
+
+    print(yellow)
+    print("Renaming files.")
+    print(reset, cyan)
+
+    files = glob.glob('temp\*.*')
+    for file in files:
+        print(f"Renaming {file}")
+        parts = file.split(".png" or ".jpg")
+        new_name = "{}-thumbnail.png".format(parts[0])
+        os.rename(file, new_name)
+
+    print(reset, yellow)
+
+    TempDirectory = "Temp"
+    FinishedDirectory = "Finished"
+
+    print("Copying original files")
+    copy_tree(FromDirectory, FinishedDirectory)
+
+    print("Copying renamed files")
+    copy_tree(TempDirectory, FinishedDirectory)
+
+    cleanup1 = glob.glob("Prep\\*.*")
+    cleanup2 = glob.glob("Temp\\*.*")
+
+    print("Cleaning up prep folder")
+    for file in cleanup1:
+        os.remove(file)
+
+    print("Cleaning up temp folder")
+    for file in cleanup2:
+        os.remove(file)
+
+    print(reset, green)
+    input("Done!\nYou can now close the program")
+
+
+file_conversion()
